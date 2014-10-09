8cc64bbbcfa729348e48df7cb3c00722937df8f1
Simon Walker <s.r.walker101@googlemail.com>
2014-10-09 21:12:54 +0100

    Add install_requires

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name='paranal_query',
     author='Simon Walker',
     author_email='s.r.walker101@googlemail.com',
-    packages=find_packages())
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'peewee',
+    ],
+)
