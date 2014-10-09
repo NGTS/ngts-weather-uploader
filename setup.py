e6bb644c88d850766700341611ef3d8abf819602
Simon Walker <s.r.walker101@googlemail.com>
2014-10-09 21:10:24 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paranal_query',
+    author='Simon Walker',
+    author_email='s.r.walker101@googlemail.com',
+    packages=find_packages())
