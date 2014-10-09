0835619220f2133c626245fb3e673f9f3cd73e52
Simon Walker <s.r.walker101@googlemail.com>
2014-10-09 23:44:03 +0100

    Add simple main script

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from query import Query
+from paranal_query.query import *
+from paranal_query.upload import upload_from_request, Measurement
+import vcr
+import datetime
 
 
 def main():
-    paranal_weather = Query.fetch_for_night()
+    r = query_for_night(datetime.date(2014, 10, 7))
+    data = parse_query_response(r.text)
+    upload_from_request(r)
+
 
 if __name__ == '__main__':
     main()
