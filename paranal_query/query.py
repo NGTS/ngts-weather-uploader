e40c0caff073f201ac0b691cf171a6bef94c3352
Simon Walker <s.r.walker101@googlemail.com>
2014-10-09 21:44:01 +0100

    Returning basic query

--- paranal_query/query.py
+++ paranal_query/query.py
@@ -1,13 +1,26 @@
 import requests
+import datetime
+
+ROOT_URL = 'http://archive.eso.org/wdb/wdb/eso/meteo_paranal/query'
+
+session = requests.Session()
 
 
-class Query(object):
-    ROOT_URL = 'http://archive.eso.org/wdb/wdb/eso/meteo_paranal/query'
+def query_for_night(night=None):
+    payload = {
+        'night': night if night is not None else datetime.date.today(),
+        'wdbo': 'csv/download',
+        'max_rows_returned': 1000000,
+    }
 
-    def __init__(self):
-        self.night = None
-        self.session = requests.Session()
+    fields = [
+        'tab_interval', 'tab_t1', 'tab_t2', 'tab_t3', 'tab_td1',
+        'tab_td2', 'tab_rh1', 'tab_rh2', 'tab_pr', 'tab_prq', 'tab_ws1',
+        'tab_ws2', 'tab_wd1', 'tab_wd2', 'tab_wsu', 'tab_wsv', 'tab_wsw',
+        'tab_dus1', 'tab_dul1', 'tab_dus2', 'tab_dul2',
+    ]
 
-    @classmethod
-    def fetch_for_night(cls, night=None):
-        return cls()
+    for field in fields:
+        payload[field] = True
+
+    return session.post(ROOT_URL, data=payload)
--- testing/test_query.py
+++ testing/test_query.py
@@ -1,5 +1,15 @@
-from paranal_query.query import Query
+from paranal_query.query import query_for_night
+import datetime
+import pytest
 
-def test_query_class_constructor():
-    query = Query.fetch_for_night()
-    assert isinstance(query, Query)
+
+@pytest.fixture(scope='session')
+def query_response():
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    return query_for_night()
+
+
+
+def test_query_for_night_status(query_response):
+    assert query_response.status_code == 200
+
