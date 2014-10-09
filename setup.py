1aaf2d96353c65c94e87654cd72e806039b13bb8
Simon Walker <s.r.walker101@googlemail.com>
2014-10-09 21:13:20 +0100

    Add version string

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='paranal_query',
     author='Simon Walker',
     author_email='s.r.walker101@googlemail.com',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'requests',
