8ee41b488f4b10c001e96415192dfffd07be661b
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 15:02:53 +0100

    Add query_response fixture

--- testing/test_ambient.py
+++ testing/test_ambient.py
@@ -1,9 +1,17 @@
 from paranal_query.query import Query
 import pytest
+import vcr
 
 @pytest.fixture
 def query_instance():
     return Query('ambient')
+
+@pytest.fixture
+def query_response(query_instance):
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    with vcr.use_cassette('testing/fixtures/ambient.yaml'):
+        return query_instance.query_for_night()
+
 
 def test_construction(query_instance):
     assert isinstance(query_instance, Query)
