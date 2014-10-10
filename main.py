a6f8468975d91b910ca6c1ba47b86ee80eee03c1
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 15:40:53 +0100

    Main script uploads todays data

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from paranal_query.query import query_for_night, parse_query_response
+from paranal_query.query import Query
 from paranal_query.upload import upload_from_request
 import datetime
 
 
 def main():
-    r = query_for_night(datetime.date(2014, 10, 7))
-    upload_from_request(r)
+    for database in ['weather', 'ambient']:
+        query = Query(database)
+        r = query.for_night()
+        upload_from_request(query, r)
 
 
 if __name__ == '__main__':
