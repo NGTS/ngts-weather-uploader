005ad12b1a972ca433d48ef1a1b02250f58d4538
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 16:09:54 +0100

    Move main script into subdir

--- paranal_query/__main__.py
+++ paranal_query/__main__.py
@@ -6,9 +6,10 @@ import datetime
 
 
 def main():
+    start_date = datetime.date(2012, 2, 3)
+    end_date = datetime.date(2012, 2, 5)
     for database in ['weather', 'ambient']:
-        query = Query(database)
-        query.upload()
+        query = Query(database).upload(start_date=start_date, end_date=end_date)
 
 
 if __name__ == '__main__':
