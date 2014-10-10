8c2659618044fd5e9212f595caf38495eca87fc7
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 15:14:44 +0100

    Test data type casting

--- testing/test_ambient.py
+++ testing/test_ambient.py
@@ -23,6 +23,7 @@ def test_construction(query_instance):
 def test_query_for_night_status(query_response):
     assert query_response.status_code == 200
 
+
 def test_rename_columns(query_instance):
 
     data = [{
@@ -33,4 +34,16 @@ def test_rename_columns(query_instance):
     assert list(query_instance.rename_columns(data)) == [{
         'tau0': 1,
         'seeing': 2,
+    }]
+
+
+def test_cast_data_types(query_instance):
+    data = [{
+        'night': '2014-10-09 12:00:33',
+        'airmass': '1.2',
+    }]
+
+    assert list(query_instance.cast_data_types(data)) == [{
+        'night': datetime.datetime(2014, 10, 9, 12, 0, 33),
+        'airmass': 1.2,
     }]
