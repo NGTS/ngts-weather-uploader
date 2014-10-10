1849d04fcec4e6bdb1b647dc410c639f2192d3e4
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 15:12:48 +0100

    Testing query interface

--- paranal_query/ambient.py
+++ paranal_query/ambient.py
@@ -7,5 +7,21 @@ COLUMN_NAME_MAP = {
 
 COLUMN_DATA_CASTERS = {}
 
-PAYLOAD = {}
+PAYLOAD = {
+    'wdbo': 'csv/download',
+    'max_rows_returned': 1000000,
+}
+
+fields = [
+'tab_interval',
+'tab_ra',
+'tab_dec',
+'tab_fwhm',
+'tab_airmass',
+'tab_rfl',
+'tab_tau',
+'tab_tet',
+]
+
+PAYLOAD.update({field: True for field in fields})
 
--- testing/test_ambient.py
+++ testing/test_ambient.py
@@ -1,17 +1,24 @@
 from paranal_query.query import Query
 import pytest
+import datetime
 import vcr
+
 
 @pytest.fixture
 def query_instance():
     return Query('ambient')
 
+
 @pytest.fixture
 def query_response(query_instance):
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    d = datetime.date(2011, 3, 15)
     with vcr.use_cassette('testing/fixtures/ambient.yaml'):
-        return query_instance.query_for_night()
+        return query_instance.query_for_night(d)
 
 
 def test_construction(query_instance):
     assert isinstance(query_instance, Query)
+
+
+def test_query_for_night_status(query_response):
+    assert query_response.status_code == 200
