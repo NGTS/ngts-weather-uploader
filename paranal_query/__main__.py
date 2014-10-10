c85509ea3f94ba9a409a951fe4afa87c3a892fe4
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 16:35:54 +0100

    Handling command line arguments

--- paranal_query/__main__.py
+++ paranal_query/__main__.py
@@ -12,7 +12,32 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    return parser.parse_args()
+    parser.add_argument('--night', required=False)
+    parser.add_argument('--start_date', required=False)
+    parser.add_argument('--end_date', required=False)
+    args = parser.parse_args()
+
+    validate_args(args)
+    return args
+
+
+
+def validate_args(args):
+    '''
+    Ensure either:
+
+        * no date arguments are given and assume todays date,
+        * night and not start date or end date,
+        * start date and not night or end_date
+        * start_date and end_date and not night
+    '''
+    if ((not args.night and not args.start_date and not args.end_date) or
+        (args.night and not args.start_date and not args.end_date) or
+        (args.start_date and not args.night)):
+        return
+    else:
+        raise RuntimeError("Invalid date combination passed")
+
 
 
 if __name__ == '__main__':
