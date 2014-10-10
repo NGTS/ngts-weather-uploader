9e56f437c71b48f6bd5cf3c045650195e0032bc3
Simon Walker <s.r.walker101@googlemail.com>
2014-10-10 16:13:41 +0100

    Add console script

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+package_name = 'paranal_query'
+
 setup(
-    name='paranal_query',
+    name=package_name,
     author='Simon Walker',
     author_email='s.r.walker101@googlemail.com',
     version='0.0.1',
@@ -10,4 +12,10 @@ setup(
         'requests',
         'peewee',
     ],
+    entry_points={
+        'console_scripts': [
+            'upload_paranal_metadata.py = {package_name}.__main__:main'.format(
+                package_name=package_name),
+        ],
+    },
 )
