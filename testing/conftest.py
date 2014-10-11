d32ca669f20160f1117f4cf156eddf619b9aac63
Simon Walker <s.r.walker101@googlemail.com>
2014-10-11 16:16:43 +0100

    New database for each test

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 
-@pytest.fixture(autouse=True, scope='session')
-def set_database_url():
-    os.environ['DATABASE_URL'] = 'sqlite:////tmp/test.db'
+@pytest.fixture(autouse=True)
+def set_database_url(tmpdir):
+    path = tmpdir.join('test.db')
+    os.environ['DATABASE_URL'] = 'sqlite://{}'.format(path)
