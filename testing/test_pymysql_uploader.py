4a207ea3d3cee457b9098d49ca5b1295026459a1
Simon Walker <s.r.walker101@googlemail.com>
2015-12-21 10:51:38 +0000

    Uploading data

--- testing/test_pymysql_uploader.py
+++ testing/test_pymysql_uploader.py
@@ -1,6 +1,16 @@
 import pymysql
 import pytest
+import mock
 from paranal_query.upload import PyMySQLUploader
+from paranal_query.query import Query
+import vcr
+from contextlib import contextmanager
+
+
+@contextmanager
+def cassette():
+    with vcr.use_cassette('testing/fixtures/ambient.yaml'):
+        yield
 
 
 @pytest.fixture
@@ -14,7 +24,8 @@ def test_it_creates_the_initial_table(connection):
     cursor.execute('drop table if exists eso_paranal_ambient')
     cursor.execute('drop table if exists eso_paranal_weather')
 
-    uploader = PyMySQLUploader(None, None)
+    query = mock.Mock(query_type='ambient')
+    uploader = PyMySQLUploader(query=query, request=None)
 
     cursor.execute('show tables')
     tables = {row[0] for row in cursor.fetchall()}
@@ -39,26 +50,50 @@ def test_it_creates_the_initial_table(connection):
     rows = [row[:2] for row in cursor.fetchall()]
     assert rows == [
         ('id', 'int(11)'),
-        ('air_pressure_2m','float'),
-        ('particule_count_20m','float'),
-        ('humidity_30m','float'),
-        ('5u_particule_count_20m','float'),
-        ('dewtemp_30m','float'),
-        ('ambient_temp','float'),
-        ('ground_temp','float'),
-        ('wind_direction_30m','float'),
-        ('wind_speed_10m','float'),
-        ('wind_speed_w','float'),
-        ('wind_speed_30m','float'),
-        ('wind_speed_u','float'),
-        ('wind_speed_v','float'),
-        ('5u_particule_count_30m','float'),
-        ('ambient_temp_30m','float'),
-        ('night','datetime'),
-        ('dewtemp_2m','float'),
-        ('interval','int(11)'),
-        ('humidity_2m','float'),
-        ('wind_direction_10m','float'),
-        ('pressure_sea_level','float'),
-        ('particule_count_30m','float'),
+        ('air_pressure_2m', 'float'),
+        ('particule_count_20m', 'float'),
+        ('humidity_30m', 'float'),
+        ('5u_particule_count_20m', 'float'),
+        ('dewtemp_30m', 'float'),
+        ('ambient_temp', 'float'),
+        ('ground_temp', 'float'),
+        ('wind_direction_30m', 'float'),
+        ('wind_speed_10m', 'float'),
+        ('wind_speed_w', 'float'),
+        ('wind_speed_30m', 'float'),
+        ('wind_speed_u', 'float'),
+        ('wind_speed_v', 'float'),
+        ('5u_particule_count_30m', 'float'),
+        ('ambient_temp_30m', 'float'),
+        ('night', 'datetime'),
+        ('dewtemp_2m', 'float'),
+        ('interval', 'int(11)'),
+        ('humidity_2m', 'float'),
+        ('wind_direction_10m', 'float'),
+        ('pressure_sea_level', 'float'),
+        ('particule_count_30m', 'float'),
     ]
+
+
+def test_upload_ambient(connection):
+    cursor = connection.cursor()
+    cursor.execute('drop table if exists eso_paranal_ambient')
+
+    with cassette():
+        query = Query('ambient', uploader_class=PyMySQLUploader)
+        query.upload('2013-01-01')
+
+    cursor.execute('select * from eso_paranal_ambient')
+    assert cursor.rowcount >= 1
+
+
+def test_upload_weather(connection):
+    cursor = connection.cursor()
+    cursor.execute('drop table if exists eso_paranal_weather')
+
+    with vcr.use_cassette('testing/fixtures/weather.yml'):
+        query = Query('weather', uploader_class=PyMySQLUploader)
+        query.upload('2013-01-01')
+
+    cursor.execute('select * from eso_paranal_weather')
+    assert cursor.rowcount >= 1
