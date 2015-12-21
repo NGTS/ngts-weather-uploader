511e3f8d7ee9e818a474b6a1213fe97cb0b0d4bf
Simon Walker <s.r.walker101@googlemail.com>
2015-12-21 10:29:13 +0000

    Extract column descriptions

--- paranal_query/upload.py
+++ paranal_query/upload.py
@@ -4,6 +4,7 @@ import pymysql
 from .logger import logger
 import paranal_query.weather as paranal_weather
 import paranal_query.ambient as paranal_ambient
+from collections import OrderedDict
 
 database_proxy = peewee.Proxy()
 
@@ -121,6 +122,48 @@ class PyMySQLUploader(Uploader):
     DB_USER = 'simon'
     DB_DBNAME = 'test'
 
+    AMBIENT_COLUMNS = OrderedDict([
+        ('id', 'integer primary key auto_increment'),
+        ('airmass', 'float'),
+        ('ra', 'float'),
+        ('tau0', 'float'),
+        ('night', 'datetime'),
+        ('interval', 'integer'),
+        ('theta0', 'float'),
+        ('declination', 'float'),
+        ('flux_rms', 'float'),
+        ('seeing', 'float'),
+    ])
+
+    WEATHER_COLUMNS = OrderedDict([
+        ('id', 'integer primary key auto_increment'),
+        ('air_pressure_2m', 'float'),
+        ('particule_count_20m', 'float'),
+        ('humidity_30m', 'float'),
+        ('5u_particule_count_20m', 'float'),
+        ('dewtemp_30m', 'float'),
+        ('ambient_temp', 'float'),
+        ('ground_temp', 'float'),
+        ('wind_direction_30m', 'float'),
+        ('wind_speed_10m', 'float'),
+        ('wind_speed_w', 'float'),
+        ('wind_speed_30m', 'float'),
+        ('wind_speed_u', 'float'),
+        ('wind_speed_v', 'float'),
+        ('5u_particule_count_30m', 'float'),
+        ('ambient_temp_30m', 'float'),
+        ('night', 'datetime'),
+        ('dewtemp_2m', 'float'),
+        ('interval', 'integer'),
+        ('humidity_2m', 'float'),
+        ('wind_direction_10m', 'float'),
+        ('pressure_sea_level', 'float'),
+        ('particule_count_30m', 'float'),
+    ])
+
+    COLUMN_NAME_MAP = {'eso_paranal_ambient': AMBIENT_COLUMNS,
+                       'eso_paranal_weather': WEATHER_COLUMNS}
+
     def __init__(self, query, request):
         self.connection = self.connect()
         self.cursor = self.connection.cursor()
@@ -131,48 +174,21 @@ class PyMySQLUploader(Uploader):
         return pymysql.connect(host=cls.DB_HOST, user=cls.DB_USER,
                                db=cls.DB_DBNAME)
 
+    def construct_single_table(self, key):
+        column_descriptions = []
+        for (name, type) in self.COLUMN_NAME_MAP[key].items():
+            row = '`{name}` {type}'.format(name=name, type=type)
+            column_descriptions.append(row)
+        self.cursor.execute('''create table if not exists {key} (
+            {columns}
+        )'''.format(key=key, columns=','.join(column_descriptions)))
+
+
     def build_database(self):
         logger.debug('Initialising database tables')
-        self.cursor.execute(
-            '''create table if not exists eso_paranal_ambient (
-            `id` integer primary key auto_increment,
-            `airmass` float,
-            `ra` float,
-            `tau0` float,
-            `night` datetime,
-            `interval` integer,
-            `theta0` float,
-            `declination` float,
-            `flux_rms` float,
-            `seeing` float
-        )''')
+        for key in self.COLUMN_NAME_MAP.keys():
+            self.construct_single_table(key)
 
-        self.cursor.execute(
-            '''create table if not exists eso_paranal_weather (
-            `id` integer primary key auto_increment,
-            `air_pressure_2m` float,
-            `particule_count_20m` float,
-            `humidity_30m` float,
-            `5u_particule_count_20m` float,
-            `dewtemp_30m` float,
-            `ambient_temp` float,
-            `ground_temp` float,
-            `wind_direction_30m` float,
-            `wind_speed_10m` float,
-            `wind_speed_w` float,
-            `wind_speed_30m` float,
-            `wind_speed_u` float,
-            `wind_speed_v` float,
-            `5u_particule_count_30m` float,
-            `ambient_temp_30m` float,
-            `night` datetime,
-            `dewtemp_2m` float,
-            `interval` integer,
-            `humidity_2m` float,
-            `wind_direction_10m` float,
-            `pressure_sea_level` float,
-            `particule_count_30m` float
-        )''')
 
     def upload(self):
         pass
