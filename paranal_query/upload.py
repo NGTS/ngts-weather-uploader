e551a46450416f5845e4aabc6315cc11cc8c787e
Simon Walker <s.r.walker101@googlemail.com>
2015-12-21 10:19:06 +0000

    Add uploader that creates its own data

--- paranal_query/upload.py
+++ paranal_query/upload.py
@@ -1,5 +1,6 @@
 import peewee
 import pw_database_url as db_uri
+import pymysql
 from .logger import logger
 import paranal_query.weather as paranal_weather
 import paranal_query.ambient as paranal_ambient
@@ -113,3 +114,65 @@ class Uploader(object):
             if row_count >= self.query.max_rows:
                 logger.warning('Not all rows uploaded, consider querying for a '
                                'smaller date range or raising `query.max_rows`')
+
+
+class PyMySQLUploader(Uploader):
+    DB_HOST = 'localhost'
+    DB_USER = 'simon'
+    DB_DBNAME = 'test'
+
+    def __init__(self, query, request):
+        self.connection = self.connect()
+        self.cursor = self.connection.cursor()
+        super(PyMySQLUploader, self).__init__(query, request)
+
+    @classmethod
+    def connect(cls):
+        return pymysql.connect(host=cls.DB_HOST, user=cls.DB_USER,
+                               db=cls.DB_DBNAME)
+
+    def build_database(self):
+        logger.debug('Initialising database tables')
+        self.cursor.execute(
+            '''create table if not exists eso_paranal_ambient (
+            `id` integer primary key auto_increment,
+            `airmass` float,
+            `ra` float,
+            `tau0` float,
+            `night` datetime,
+            `interval` integer,
+            `theta0` float,
+            `declination` float,
+            `flux_rms` float,
+            `seeing` float
+        )''')
+
+        self.cursor.execute(
+            '''create table if not exists eso_paranal_weather (
+            `id` integer primary key auto_increment,
+            `air_pressure_2m` float,
+            `particule_count_20m` float,
+            `humidity_30m` float,
+            `5u_particule_count_20m` float,
+            `dewtemp_30m` float,
+            `ambient_temp` float,
+            `ground_temp` float,
+            `wind_direction_30m` float,
+            `wind_speed_10m` float,
+            `wind_speed_w` float,
+            `wind_speed_30m` float,
+            `wind_speed_u` float,
+            `wind_speed_v` float,
+            `5u_particule_count_30m` float,
+            `ambient_temp_30m` float,
+            `night` datetime,
+            `dewtemp_2m` float,
+            `interval` integer,
+            `humidity_2m` float,
+            `wind_direction_10m` float,
+            `pressure_sea_level` float,
+            `particule_count_30m` float
+        )''')
+
+    def upload(self):
+        pass
